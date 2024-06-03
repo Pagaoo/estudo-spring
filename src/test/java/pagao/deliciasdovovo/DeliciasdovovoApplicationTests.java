790b62e498a2f62fe0547bedb23ab81f32021597
Pagão <gabrielpaganinibarroso@hotmail.com>
2024-06-03 15:52:19 -0300

    config inicial

---
+++ src/test/java/pagao/deliciasdovovo/DeliciasdovovoApplicationTests.java
@@ -0,0 +1,13 @@
+package pagao.deliciasdovovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliciasdovovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
