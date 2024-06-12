857f76846f73a0c77013b9da03972a1b0de282ae
Pagão <gabrielpaganinibarroso@hotmail.com>
2024-06-12 16:52:33 -0300

    uuid para id

---
+++ src/main/java/pagao/deliciasdovovo/entities/Customer.java
@@ -0,0 +1,20 @@
+package pagao.deliciasdovovo.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+}
