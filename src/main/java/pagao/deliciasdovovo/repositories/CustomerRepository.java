d087cf2603657ede8b8401c0964fd673eecd728e
Pagão <gabrielpaganinibarroso@hotmail.com>
2024-06-17 14:57:02 -0300

    feat: creating repository interface to use crud functions

---
+++ src/main/java/pagao/deliciasdovovo/repositories/CustomerRepository.java
@@ -0,0 +1,9 @@
+package pagao.deliciasdovovo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pagao.deliciasdovovo.entities.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
