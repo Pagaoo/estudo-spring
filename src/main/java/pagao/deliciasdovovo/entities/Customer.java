bd12329bf4f111b0d07e22ea28e87f6a024069dc
Pagão <gabrielpaganinibarroso@hotmail.com>
2024-06-17 14:56:11 -0300

    feat: creating dto to crate a customer function

--- src/main/java/pagao/deliciasdovovo/entities/Customer.java
+++ src/main/java/pagao/deliciasdovovo/entities/Customer.java
@@ -2,19 +2,38 @@ package pagao.deliciasdovovo.entities;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import pagao.deliciasdovovo.dtos.CustomerDto;
 
 @Entity
 @Table(name = "customers")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank
     private String firstName;
+    @NotBlank
     private String lastName;
+    @NotBlank
+    @Column(unique = true)
     private String email;
+    @NotBlank
+    @Column(unique = true)
     private String phone;
+
+    public Customer(CustomerDto customerDto) {
+        this.firstName = customerDto.firstName();
+        this.lastName = customerDto.lastName();
+        this.email = customerDto.email();
+        this.phone = customerDto.phone();
+    }
 }
