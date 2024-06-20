035b5d50951a7c913d3c772d3fd92680c3a3a0cd
Pagão <gabrielpaganinibarroso@hotmail.com>
2024-06-20 18:45:20 -0300

    feat: created a method signature

--- src/main/java/pagao/deliciasdovovo/repositories/CustomerRepository.java
+++ src/main/java/pagao/deliciasdovovo/repositories/CustomerRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pagao.deliciasdovovo.entities.Customer;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Customer getCustomerById(Long id);
 }
