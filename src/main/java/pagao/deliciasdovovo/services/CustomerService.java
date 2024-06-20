52d102a541ac107451fe31c8347b3cf3c92d8031
Pagão <gabrielpaganinibarroso@hotmail.com>
2024-06-20 18:44:28 -0300

    feat: created validate transaction

--- src/main/java/pagao/deliciasdovovo/services/CustomerService.java
+++ src/main/java/pagao/deliciasdovovo/services/CustomerService.java
@@ -5,10 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import pagao.deliciasdovovo.dtos.CustomerDto;
 import pagao.deliciasdovovo.entities.Customer;
+import pagao.deliciasdovovo.enums.UserType;
 import pagao.deliciasdovovo.repositories.CustomerRepository;
 
+import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CustomerService {
@@ -26,13 +27,28 @@ public class CustomerService {
     }
 
 
-    public Optional<Customer> getCustomerById(Long id) throws Exception {
+    public Customer getCustomerById(Long id) {
         logger.info("[Customer Service] getting customer by id: {}", id);
-        return customerRepository.findById(id);
+        return customerRepository.getCustomerById(id);
     }
 
     public Customer CreateCustomer(CustomerDto customerDto) {
             Customer newCustomer = new Customer(customerDto);
             return customerRepository.save(newCustomer);
+    }
+
+    public void validateTransaction(Customer sender, BigDecimal transactionValue) throws Exception {
+        if (sender.getUserType() == UserType.LOJA) {
+            logger.info("[Customer Service] Checking if user type can make a transaction");
+            throw new Exception("LOJAS não podem fazer transações de compra");
+        }
+
+        if (sender.getBalance().compareTo(transactionValue) < 0 ) {
+            throw new Exception("Valor insuficiente para realizar transação");
+        }
+    }
+
+    public Customer saveCustomer(Customer customer) {
+        return this.saveCustomer(customer);
     }
 }
