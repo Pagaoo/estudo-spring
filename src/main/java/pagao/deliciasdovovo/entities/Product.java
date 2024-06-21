3225bdaeb0d05536179b2c210354631ac493872e
Pagão <gabrielpaganinibarroso@hotmail.com>
2024-06-21 17:51:18 -0300

    feat: created product entity

---
+++ src/main/java/pagao/deliciasdovovo/entities/Product.java
@@ -0,0 +1,26 @@
+package pagao.deliciasdovovo.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tables")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private String productType;
+    private BigDecimal price;
+    private int quantity;
+}
