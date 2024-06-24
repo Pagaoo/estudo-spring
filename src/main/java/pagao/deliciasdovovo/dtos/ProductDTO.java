c1ebd050129dd629d091f335d2843c4c8ca1312f
Pagão <gabrielpaganinibarroso@hotmail.com>
2024-06-24 17:45:01 -0300

    feat: criada a classe de DTO para produto

---
+++ src/main/java/pagao/deliciasdovovo/dtos/ProductDTO.java
@@ -0,0 +1,6 @@
+package pagao.deliciasdovovo.dtos;
+
+import java.math.BigDecimal;
+
+public record ProductDTO(String name, String description, String productType, BigDecimal price, int quantity) {
+}
